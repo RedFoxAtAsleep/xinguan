cde6c875df94e02174cd988fe5cc708a525121ee
zhaojinhui <bbxxone@qq.com>
2020-07-04 14:26:17 +0800

    Update.

--- synchronize.py
+++ synchronize.py
@@ -3,96 +3,13 @@ import logging
 import time
 import akshare as ak
 import sqlalchemy as db  # Version Check
-from sqlalchemy import create_engine  # Connecting
-from sqlalchemy import MetaData, Table, Sequence, Column, Integer, String, PrimaryKeyConstraint, ForeignKey  # Define and Create Tables
+import pandas as pd
+import logging
+from sqlalchemy import create_engine
+from sqlalchemy import MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import pandas as pd
-
-# logging.basicConfig()函数是一个一次性的简单配置工具使
-logging.basicConfig(**{
-    'level': logging.INFO,
-    'format': "%(asctime)s - %(levelname)s - %(message)s",
-    'datefmt': "%m/%d/%Y %H:%M:%S %p"
-})
-
-
-# orm: metadata/engine/session
-# metadata
-
-Base = declarative_base()
-
-
-class District(Base):
-    __tablename__ = 'district'
-    __table_args__ = {'extend_existing': True}
-    id = Column(Integer, Sequence('district_id_seq'), primary_key=True)
-    province = Column(String(32))
-    city = Column(String(32))
-    district = Column(String(32))
-
-    def __repr__(self):
-        return '<District(province={province};city={city};district={district})>'.format(**{
-            'province': self.province,
-            'city': self.city,
-            'district': self.district
-        })
-
-# SQLAlchemy的MapperObject必须要配置主键
-# 配置没有主键的MapperObject，联合主键，如所有的字段构成主键
-
-# 在Sqlalchemy中使用联合主键
-# https://zhuanlan.zhihu.com/p/106568544
-# ORM配置 | 翻译
-# https://www.osgeo.cn/sqlalchemy/faq/ormconfiguration.html
-# ORM Configuration
-# https://docs.sqlalchemy.org/en/13/faq/ormconfiguration.html#how-do-i-map-a-table-that-has-no-primary-key
-
-# 联合主键定义方式一
-# class District(Base):
-#     __tablename__ = 'district'
-#     __table_args__ = {'extend_existing': True}
-#     province = Column(String(32), primary_key=True)
-#     city = Column(String(32), primary_key=True)
-#     district = Column(String(32), primary_key=True)
-#
-#     def __repr__(self):
-#         return '<District(province={province};city={city};district={district})>'.format(**{
-#             'province': self.province,
-#             'city': self.city,
-#             'district': self.district
-#         })
-
-
-# 联合主键定义方式二
-# class District(Base):
-#     __tablename__ = 'district'
-#     __table_args__ = (
-#         PrimaryKeyConstraint('province', 'city', 'district'),
-#         {'extend_existing': True},
-#     )
-#     province = Column(String(32))
-#     city = Column(String(32))
-#     district = Column(String(32))
-#
-#     def __repr__(self):
-#         return '<District(province={province};city={city};district={district})>'.format(**{
-#             'province': self.province,
-#             'city': self.city,
-#             'district': self.district
-#         })
-
-
-class DistrictInfo(Base):
-    __tablename__ = 'district_info'
-    __table_args__ = {'extend_existing': True}
-    id = Column(Integer, Sequence('district_info_id_seq'), primary_key=True)
-    province = Column(String(32))
-    city = Column(String(32))
-    district = Column(String(32))
-    show_address = Column(String(64))
-    full_address = Column(String(64))
-    cnt_sum_certain = Column(Integer)  # -1: 表示有确诊但是确诊人数不详
+from data import District, DistrictInfo, Base
 
 
 def synchronize_district_data(manner='init', endurance=100):
@@ -200,16 +117,6 @@ if __name__ == '__main__':
     })
     engine = db.create_engine(connection_uri)
 
-    Base.metadata.create_all(engine)
-    # synchronize_district_data()
-
-
-
-
-
-
-
-
-
-
-
+    # District.__table__.create(engine)
+    # DistrictInfo.__table__.create(engine)
+    synchronize_district_data()
