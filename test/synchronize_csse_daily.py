39427aa0691877a48d2701bf90289889de7c8aec
zhaojinhui <bbxxone@qq.com>
2020-07-06 16:14:59 +0800

    导入上级文件。

--- test/synchronize_csse_daily.py
+++ test/synchronize_csse_daily.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
-import sqlalchemy as db  # Version Check
+import sqlalchemy as db
 import logging
 import sys
 sys.path.append("..")
 from synchronize import synchronize_csse_daily
 
-if __name__ == '__main__':
-    # engine
-    logging.info('Create independent engine.')
-    connection_uri = "mysql+pymysql://{user}:{password}@{host}:{port}/{dbname}".format(**{
-        "user": "root",
-        "password": "mysql123456",
-        "host": "localhost",
-        "port": "3306",
-        "dbname": "xinguan"
-    })
-    engine = db.create_engine(connection_uri)
-    synchronize_csse_daily(engine)
+
+logging.info('Create independent engine.')
+connection_uri = "mysql+pymysql://{user}:{password}@{host}:{port}/{dbname}".format(**{
+    "user": "root",
+    "password": "mysql123456",
+    "host": "localhost",
+    "port": "3306",
+    "dbname": "xinguan"
+})
+engine = db.create_engine(connection_uri)
+synchronize_csse_daily(engine)
--- test/synchronize_qq_community.py
+++ test/synchronize_qq_community.py
@@ -2,18 +2,17 @@
 import sqlalchemy as db
 import logging
 import sys
+
 sys.path.append("..")
 from synchronize import synchronize_qq_community
 
-if __name__ == '__main__':
-    # engine
-    logging.info('Create independent engine.')
-    connection_uri = "mysql+pymysql://{user}:{password}@{host}:{port}/{dbname}".format(**{
-        "user": "root",
-        "password": "mysql123456",
-        "host": "localhost",
-        "port": "3306",
-        "dbname": "xinguan"
-    })
-    engine = db.create_engine(connection_uri)
-    synchronize_qq_community(engine)
+logging.info('Create independent engine.')
+connection_uri = "mysql+pymysql://{user}:{password}@{host}:{port}/{dbname}".format(**{
+    "user": "root",
+    "password": "mysql123456",
+    "host": "localhost",
+    "port": "3306",
+    "dbname": "xinguan"
+})
+engine = db.create_engine(connection_uri)
+synchronize_qq_community(engine)
